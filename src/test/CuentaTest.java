be3e87162d82d7aed15547c5527a4ec60951d02e
Josebaena03 <josecolegioo12@gmail.com>
2025-10-15 23:22:10 +0200

    Versión 1

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -1,47 +1,13 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import pkg.Cuenta;
 
 class CuentaTest {
 
-	private static Cuenta cuenta;
-	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		cuenta = new Cuenta(0.0);
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-		cuenta.setSaldo(0.0);
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void testIngresar() {
-		cuenta.ingresar(500.0);
-		assertEquals(500.0, cuenta.getSaldo());
-	}
-	@Test
-	void testRetirar() {
-		cuenta.retirar(500.0);
-		assertEquals(-500.0, cuenta.getSaldo());
-	}
-	
-
+    @Test
+    void alCrearUnaCuentaConSaldoInicial_ElSaldoEsCorrecto() {
+        Cuenta cuenta = new Cuenta("12345", 50.0);
+        assertEquals(50.0, cuenta.getSaldo());
+    }
 }
